738f38153555c0984b68ea9e2a59c868068282df
unknown <a.sidorovich@a-sidorovich.godeltech.com>
2015-04-17 23:30:01 +0300

    Added imported database into DD.Database project

---
+++ DD.Database/dbo/Tables/Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [ID]        INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (20) NOT NULL,
+    [LastName]  VARCHAR (20) NULL,
+    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ DD.Database/dbo/Tables/PersonRecord.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PersonRecord] (
+    [ID]                  INT            IDENTITY (1, 1) NOT NULL,
+    [PersonId]            INT            NOT NULL,
+    [RecordTime]          DATETIME       NOT NULL,
+    [Insulin]             DECIMAL (4, 2) NULL,
+    [Carbohydrates]       DECIMAL (4, 2) NULL,
+    [BloodGloucoseBgm]    DECIMAL (4, 2) NULL,
+    [BloodGloucoseCgm]    DECIMAL (4, 2) NULL,
+    [BloodGloucoseCgmRaw] DECIMAL (4, 2) NULL,
+    [Note]                VARCHAR (256)  NULL,
+    CONSTRAINT [PK_PersonRecord] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_PersonRecord_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([ID])
+);
+
